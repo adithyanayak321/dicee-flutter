2a27f5cbb300a3f394a85315b239adbdaedee2b4
Adithya Nayak <adithyanayak321@gmail.com>
2021-05-21 18:49:48 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,18 +8,64 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DicePage(
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key key}) : super(key: key);
+
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  @override
+  int diceNumber=1;
+  int diceNumber2=1;
   Widget build(BuildContext context) {
-    return Container();
+void randomNumbers(){
+  diceNumber=Random().nextInt(6)+1;
+  diceNumber2=Random().nextInt(6)+1;
+}
+    return Container(
+        child:Center(
+          child: Row(
+            children: [Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  onPressed: (){
+                  setState(() {
+                   randomNumbers();
+                  });
+                  },
+                  style: TextButton.styleFrom(padding: EdgeInsets.all(0)),
+                  child: Image.asset('images/dice$diceNumber.png'),),
+              ),
+            ),
+              Expanded(
+                child:Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                      onPressed: (){
+                        setState(() {
+                        randomNumbers();
+                        });
+                      },
+                      style: TextButton.styleFrom(padding: EdgeInsets.all(0),),
+                      child: Image.asset('images/dice$diceNumber2.png')),
+                ),
+              ),],
+          ),
+        )
+    );;
   }
 }
